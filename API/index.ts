050bd9a143e4fc93148a385661fce70c97923e76
rponomarev <rponomarev@webinar.ru>
2024-06-19 16:28:32 +0300

    lab-7 ("Added search")

--- API/index.ts
+++ API/index.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: 'http://192.168.1.143:8000'})
+export const axiosInstance = axios.create({ baseURL: 'http://localhost:8000'})
+// export const axiosInstance = axios.create({ baseURL: 'http://192.168.1.143:8000'})
--- screens/ShopScreen.tsx
+++ screens/ShopScreen.tsx
@@ -1,6 +1,6 @@
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, TextInput, Text} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {axiosInstance} from '@/API';
 import {setDevices} from '@/store/deviceSlice';
 import DeviceCard from '../components/DeviceCard';
@@ -8,27 +8,42 @@ import DeviceCard from '../components/DeviceCard';
 // @ts-ignore
 export default function ShopScreen({navigation}) {
     const dispatch = useDispatch();
+    const [value, setValue] = useState('');
     // @ts-ignore
     const {devices} = useSelector((store) => store.device);
 
-    useEffect(() => {
-        async function getAllDevices() {
-            try{
-                await axiosInstance.get('/service/').then((response) => dispatch(setDevices(response?.data)));
-            } catch (e){
-                console.log('error', e);
-            }
+    async function getAllDevices(v: any) {
+        console.log(v);
+        try {
+            const title = v !== '' ? `?title=${v}` : '';
+            await axiosInstance
+                .get(`/service/${title}`)
+                .then((response) => dispatch(setDevices(response?.data)));
+        } catch (e) {
+            console.log('error', e);
         }
+    }
 
-        getAllDevices();
-    }, [dispatch]);
+    useEffect(() => {
+        getAllDevices(value);
+    }, [dispatch, value]);
+
+    const handleChange = (e: any) => {
+        setValue(e.target.value);
+    }
 
     return (
         <ScrollView>
             <View style={styles.page}>
-                {!!devices && devices.map((device: any) => (
+                <TextInput
+                    style={styles.input}
+                    onChange={(e) => handleChange(e)}
+                    value={value}
+                    placeholder='Введите услугу'
+                />
+                {!!devices && devices.length > 0 ? devices.map((device: any) => (
                     <DeviceCard key={device.pk} {...device} navigation={navigation}/>
-                ))}
+                )) : <Text style={styles.notFound}>Услуги не найдены</Text>}
             </View>
         </ScrollView>
     );
@@ -43,4 +58,26 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
         backgroundColor: '#fff',
     },
+    input: {
+        width: 360,
+        height: 40,
+        fontSize: 24,
+        lineHeight: 40,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#ccc',
+        margin: 'auto',
+        marginTop: 20,
+        borderRadius: 6,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    notFound: {
+        fontSize: 24,
+        lineHeight: 28,
+        textAlign: 'center',
+        margin: 'auto',
+        marginTop: 40,
+        paddingBottom: 40
+    }
 });
